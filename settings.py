587e1fdf1390f7c0a7055c2ba2abb9b9bd4ae43f
Luke <51127079+PulpCattel@users.noreply.github.com>
2020-09-21 18:58:06 +0000

    Remove useless advice

--- settings.py
+++ settings.py
@@ -2,6 +2,6 @@ settings = {
     'rpc_user': '',
     'rpc_password': '',
     'logging': 'warning',  # Possible values: False, 'warning', 'info'
-    'memory_limit': 80, # Max % of used RAM before force stop, if you have low resources you may consider lowering this
+    'memory_limit': 80, # Max % of used RAM before force stop
     'scan_limit': 15, # Max number of concurrent block scans
 }
