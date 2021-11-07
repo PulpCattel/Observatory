f83df349ea0747133990a314c34ea385fc449d55
PulpCattel <PulpCattel@users.noreply.github.com>
2021-11-07 14:16:10 +0000

    Remove obsolete code
    The Observatory has been updated significantly, this code is now obsolete.

--- settings.py
+++
@@ -1,8 +0,0 @@
-settings = {
-    'rpc_user': '',
-    'rpc_password': '',
-    'rpc_endpoint': '',  # Leave empty to use default http://127.0.0.1:8332/
-    'logging': 'warning',  # Possible values: '', 'warning', 'info' ('' means no logging)
-    'memory_limit': 80,  # Max % of used RAM before force stop
-    'scan_limit': 15,  # Max number of concurrent block scans
-}
